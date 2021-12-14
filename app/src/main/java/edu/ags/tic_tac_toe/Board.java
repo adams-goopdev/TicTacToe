498a8e5a37dca34b7707252fa23dd35a42e628bb
Adam-Schapekahm <60486516+Adam-Schapekahm@users.noreply.github.com>
2021-12-14 00:12:45 -0600

    Added switch statement for random spot

--- app/src/main/java/edu/ags/tic_tac_toe/Board.java
+++ app/src/main/java/edu/ags/tic_tac_toe/Board.java
@@ -9,6 +9,11 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.BlockingDeque;
+
 public class Board {
 
     public final static int BOARDSIZE = 3;
@@ -18,6 +23,8 @@ public class Board {
     //Array of the rectangles
     Rect[][] cells = new Rect[BOARDSIZE][BOARDSIZE];
 
+    ArrayList<String> emptySpaces = new ArrayList<>();
+
     public static final String TAG = "Board";
 
     int SIZE = 55;
@@ -25,36 +32,33 @@ public class Board {
     int viewHeight;
     int viewWidth;
 
-    public Board(){
+    public Board() {
 
     }
 
-    public Board(int width){
+    public Board(int width) {
         viewWidth = width / BOARDSIZE;
         viewHeight = viewWidth;
         SIZE = viewWidth - 3;
     }
 
-    public Board(int width, int height){
+    public Board(int width, int height) {
         viewWidth = width / BOARDSIZE;
         viewHeight = height / BOARDSIZE;
         SIZE = viewWidth - 3;
     }
 
-    public String hitTest(Point pt, String turn, Context context)
-    {
+    public String hitTest(Point pt, String turn, Context context) {
         String results = "-1";
-        for(int row = 0; row < cellValues[0].length; row ++)
-        {
-            for(int col = 0; col < cellValues[1].length; col ++)
-            {
-                if(cellValues[row][col]=="")
-                {
-                    if(cells[row][col].contains(pt.x, pt.y))
-                    {
-                        Log.d(TAG, "hitTest: Hit");
+        for (int row = 0; row < cellValues[0].length; row++) {
+            for (int col = 0; col < cellValues[1].length; col++) {
+                if (cellValues[row][col] == "e") {
+                    if (cells[row][col].contains(pt.x, pt.y)) {
+
                         cellValues[row][col] = turn;
                         results = "0";
+
+                        Log.d(TAG, "hitTest: Hit " + turn + " " + cellValues[row][col]);
                     }
                 }
             }
@@ -62,22 +66,15 @@ public class Board {
         return results;
     }
 
-    public void Draw(Canvas canvas)
-    {
+
+    public void Draw(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(Color.LTGRAY);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(10);
 
-        for(int row = 0; row < cellValues[0].length; row ++)
-        {
-            for(int col = 0; col < cellValues[1].length; col ++)
-            {
-
-                if((row + col) % 2 == 0)
-                    paint.setColor(Color.DKGRAY);
-                else
-                    paint.setColor(Color.DKGRAY);
+        for (int row = 0; row < cellValues[0].length; row++) {
+            for (int col = 0; col < cellValues[1].length; col++) {
 
                 cells[row][col] = new Rect();
                 cells[row][col].left = col * SIZE + OFFSET;
@@ -87,21 +84,219 @@ public class Board {
 
                 canvas.drawRect(cells[row][col], paint);
 
-                if(cellValues[row][col] == "1")
-                    drawTurn(canvas, cells[row][col],Color.RED);
-                else if(cellValues[row][col] == "2")
-                drawTurn(canvas, cells[row][col],Color.BLUE);
+                if (cellValues[row][col] == "O") {
+                    drawTurn(canvas, cells[row][col], "O");
 
+                    computerTurn(canvas,cells[row][col],"X");
+                }
+                else
+                {
+
+                }
             }
         }
     }
 
-    private void drawTurn(Canvas canvas, Rect rect, int color)
-    {
-        if(color == Color.RED)
-        {
+
+    public void createEmptySpaces() {
+        emptySpaces.add("0");
+        emptySpaces.add("1");
+        emptySpaces.add("2");
+        emptySpaces.add("3");
+        emptySpaces.add("4");
+        emptySpaces.add("5");
+        emptySpaces.add("6");
+        emptySpaces.add("7");
+        emptySpaces.add("8");
+    }
+
+
+    public void computerTurn(Canvas canvas,  String turn) {
+        int select = emptySpaces.size();
+        int random = new Random().nextInt(select);
+        String selectSpace = emptySpaces.get(random);
+        Paint paint = new Paint();
+        Rect rect1;
+
+
+        switch (selectSpace) {
+
+            case "0":
+
+                if(cellValues[0][0] == "e")
+                {
+                    rect1 = cells[0][0];
+                    cellValues[0][0] = "X";
+
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+
+
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+
+            case "1":
+
+                if(cellValues[1][0] == "e") {
+                    rect1 = cells[1][0];
+                    cellValues[1][0] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "2":
+
+                if(cellValues[2][0] == "e") {
+                    rect1 = cells[2][0];
+                    cellValues[2][0] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+
+                break;
+
+            case "3":
+                if(cellValues[0][1] == "e") {
+                    rect1 = cells[0][1];
+                    cellValues[0][1] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "4":
+
+                if(cellValues[1][1] == "e") {
+                    rect1 = cells[1][1];
+                    cellValues[1][1] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "5":
+
+                if(cellValues[2][1] == "e") {
+                    rect1 = cells[2][1];
+                    cellValues[2][1] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "6":
+                if(cellValues[0][2] == "e") {
+                    rect1 = cells[0][2];
+                    cellValues[0][2] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "7":
+
+                if(cellValues[1][2] == "e") {
+                    rect1 = cells[1][2];
+                    cellValues[1][2] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "8":
+                if(cellValues[2][2] == "e") {
+                    rect1 = cells[2][2];
+                    cellValues[2][2] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+        }
+
+    }
+
+    private void drawTurn(Canvas canvas, Rect rect, String turn) {
+        if (turn == "O") {
             Paint paint = new Paint();
-            paint.setColor(color);
+            paint.setColor(Color.RED);
 
             paint.setStyle(Paint.Style.STROKE);
             paint.setStrokeWidth(10);
@@ -110,21 +305,193 @@ public class Board {
             int y = rect.centerY();
 
             canvas.drawCircle(x, y, SIZE * .45f, paint);
-        }
-        else
-        {
-            //Draw the X
-            Paint paint = new Paint();
-            paint.setColor(color);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(10);
-
-            //Draw two lines that intersect within the rectangle
-            canvas.drawLine(rect.left, rect.bottom, rect.right, rect.top, paint);
-            canvas.drawLine(rect.right, rect.bottom, rect.left, rect.top, paint);
 
         }
 
+    }
+
+    public void computerTurn(Canvas canvas, Rect rect,  String turn) {
+        int select = emptySpaces.size();
+        int random = new Random().nextInt(select);
+        String selectSpace = emptySpaces.get(random);
+        Paint paint = new Paint();
+        Rect rect1;
+
+        int x = rect.centerX();
+        int y = rect.centerY();
+
+
+        switch (selectSpace) {
+
+            case "0":
+
+                if(cellValues[0][0] == "e")
+                {
+                    rect1 = cells[0][0];
+                    cellValues[0][0] = "X";
+
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+
+
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+
+            case "1":
+
+                if(cellValues[1][0] == "e") {
+                    rect1 = cells[1][0];
+                    cellValues[1][0] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "2":
+
+                if(cellValues[2][0] == "e") {
+                    rect1 = cells[2][0];
+                    cellValues[2][0] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+
+                break;
+
+            case "3":
+                if(cellValues[0][1] == "e") {
+                    rect1 = cells[0][1];
+                    cellValues[0][1] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "4":
+
+                if(cellValues[1][1] == "e") {
+                    rect1 = cells[1][1];
+                    cellValues[1][1] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "5":
+
+                if(cellValues[2][1] == "e") {
+                    rect1 = cells[2][1];
+                    cellValues[2][1] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "6":
+                if(cellValues[0][2] == "e") {
+                    rect1 = cells[0][2];
+                    cellValues[0][2] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "7":
+
+                if(cellValues[1][2] == "e") {
+                    rect1 = cells[1][2];
+                    cellValues[1][2] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+            case "8":
+                if(cellValues[2][2] == "e") {
+                    rect1 = cells[2][2];
+                    cellValues[2][2] = "X";
+                    //Draw the X
+                    paint.setColor(Color.BLUE);
+                    paint.setStyle(Paint.Style.STROKE);
+                    paint.setStrokeWidth(10);
+
+                    //Draw two lines that intersect within the rectangle
+                    canvas.drawLine(rect1.left, rect1.bottom, rect1.right, rect1.top, paint);
+                    canvas.drawLine(rect1.right, rect1.bottom, rect1.left, rect1.top, paint);
+                }
+                emptySpaces.remove(selectSpace);
+                Log.d(TAG, "computerTurn: " + selectSpace);
+                break;
+
+        }
 
     }
 
--- app/src/main/java/edu/ags/tic_tac_toe/Game.java
+++ app/src/main/java/edu/ags/tic_tac_toe/Game.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Game extends AppCompatActivity {
@@ -28,6 +29,8 @@ public class Game extends AppCompatActivity {
     Board board;
     String value;
     String[][] cellValues = new String[Board.BOARDSIZE][Board.BOARDSIZE];
+
+    Boolean PlayComputer = true;
 
 
     @Override
@@ -42,6 +45,7 @@ public class Game extends AppCompatActivity {
         board.cellValues = cellValues;
         setContentView(new DrawView(this));
 
+        board.createEmptySpaces();
     }
 
     private void initialSetup() {
@@ -50,7 +54,7 @@ public class Game extends AppCompatActivity {
         {
             for(int col = 0; col < cellValues[1].length; col ++)
             {
-                    cellValues[row][col] = "";
+                    cellValues[row][col] = "e";
             }
         }
     }
@@ -66,12 +70,13 @@ public class Game extends AppCompatActivity {
 
     private class DrawView extends View implements View.OnTouchListener {
 
-        String turn = "1";
+        String turn = "O";
 
         public DrawView(Game game) {
             super(game);
             this.setOnTouchListener(this);
         }
+
 
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -85,49 +90,45 @@ public class Game extends AppCompatActivity {
 
                 if(board.hitTest(pt, turn, this.getContext()) != "-1")
                 {
-                    turn = (turn == "1") ? "2" : "1";
-                    Log.d(TAG, "onTouch: " + Arrays.deepToString(cellValues));
-                }
-                else
-                {
-                    return false;
-                }
+                   //turn = (turn == "X") ? "O" : "X";
 
-                if (turn == "2")
-                {
-                    for(int row = 0; row < cellValues[0].length; row ++)
-                        for(int col = 0; col < cellValues[1].length; col ++)
-                            if(cellValues[row][col]=="")
-                                if(board.cells[row][col].contains(pt.x, pt.y))
-                                {
-                                    Log.d(TAG, "hitTest: Hit");
-                                    cellValues[row][col] = turn;
-                                }
+                    turn = "O";
+                    Log.d(TAG, "onTouch: " + turn);
+
+                }
+                else {
+
+                    return false;
+
                 }
                 invalidate();
+
             }
+
+            checkVictory();
+            Log.d(TAG, "onTouch: " + Arrays.deepToString(cellValues));
             return true;
         }
 
         @Override
         protected void onDraw(Canvas canvas)
         {
-            canvas.drawColor(Color.LTGRAY);
             board.Draw(canvas);
-            checkVictory();
-            Log.d(TAG, "onDraw: Test onDraw");
+
         }
 
     }
 
 
+
+
     public String checkVictory()
     {
-        String[] values = {"1", "2"};
+        String[] values = {"O", "X"};
         for(String value : values)
         {
             if(cellValues[0][0].equals(value) && cellValues[0][1].equals(value) && cellValues[0][2].equals(value) //Top row
-            ||cellValues[1][0].equals(value) && cellValues[1][1].equals(value) && cellValues[1][2].equals(value) //Middle row
+                    ||cellValues[1][0].equals(value) && cellValues[1][1].equals(value) && cellValues[1][2].equals(value) //Middle row
                     ||cellValues[2][0].equals(value) && cellValues[2][1].equals(value) && cellValues[2][2].equals(value) //bottom row
                     ||cellValues[0][0].equals(value) && cellValues[1][0].equals(value) && cellValues[2][0].equals(value) //1st col
                     ||cellValues[0][1].equals(value) && cellValues[1][1].equals(value) && cellValues[2][1].equals(value) //2nd col
@@ -138,29 +139,22 @@ public class Game extends AppCompatActivity {
             {
                 Log.d(TAG, "Victory!: " + value);
 
-                LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupView = inflater.inflate(R.layout.activity_main, null);
-                TextView tvWinner = findViewById(R.id.winner);
-
-
                 String message = "";
 
-                if(Integer.parseInt(value) == 1)
+                if(value == "O")
                    message = "O Won!";
-                else if (Integer.parseInt(value) == 2)
+                else if (value =="X")
                     message = "X Won!";
 
                 showMessage(message);
 
                 initialSetup();
-                board.cellValues = cellValues;
-                setContentView(new DrawView(this));
+               board.cellValues = cellValues;
+               setContentView(new DrawView(this));
 
                 return value;
             }
         }
-        Log.d(TAG, "checkVictory: " + Arrays.deepToString(cellValues));
-
 
         return value;
     }
